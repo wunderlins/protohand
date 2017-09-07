4b00b5b3802a8d6fd48c3b878356641dc8aeb049
Simon Wunderlin <swunderlin@gmail.com>
2017-09-07 17:20:45 +0200

    making stringlib.h easier to use in C++

--- stringlib.h
+++ stringlib.h
@@ -1,6 +1,18 @@
+#ifndef __STRINGLIB_H__
+#define __STRINGLIB_H__
 
+/* Make this header file easier to include in C++ code */
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 char *trim(char *str);
 int split(char* string, char* delim, char** result, int *count);
 int findchar(const char* allowed, const char* string);
 int find_param(char* search, char** options);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __STRINGLIB_H__ */
