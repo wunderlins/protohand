c0825c5a4a9f2099cdf5791c0915c6343ff80454
Simon Wunderlin <swunderlin@gmail.com>
2017-09-10 07:34:08 +0200

    umlaut tests

--- test/nvlist.c
+++ test/nvlist.c
@@ -22,6 +22,24 @@ int nvlist_addpair(struct nvlist_list *rep, char* key, char* value) {
 	return rep->length;
 }
 
+int nvlist_addstr(struct nvlist_list *rep, char* str, char delim) {
+	int i = 0;
+	int len = strlen(str);
+	for(i=0; i< len; i++) {
+		if (str[i] == delim) {
+			char *key = malloc(sizeof(char*) * (i+1));
+			char *val = malloc(sizeof(char*) * len);
+			strncpy(key, str, i);
+			key[i] = '\0';
+			strncpy(val, str+i+1, len);
+			
+			return nvlist_addpair(rep, key, val);
+		}
+	}
+	
+	return -1;
+}
+
 struct nvlist_list nvlist_create(int size) {
 	struct nvlist_list rep;
 	rep.items = malloc(sizeof(struct nvlist_pair *) * size);
@@ -50,6 +68,10 @@ int main(int argc, char** argv, char **envp) {
 	
 	nvlist_addpair(&rep, "k1", "v1");
 	nvlist_addpair(&rep, "k2", "v2");
+	nvlist_addstr(&rep, "k3&v3", '&');
+	
+	// FIXME: umlauts don't work on W10 msys with a us codepage
+	//printf("”\n"); // works if file uses CP 'OEM 852' 
 	
 	int i;
 	for (i=0; i<rep.length; i++) {
