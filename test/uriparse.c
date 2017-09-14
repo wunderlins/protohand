e2c1234e8158b915830395da59b07b1499cc42e3
Simon Wunderlin <swunderlin@gmail.com>
2017-09-14 18:47:48 +0200

    added unit test methods

--- test/uriparse.c
+++ test/uriparse.c
@@ -3,9 +3,21 @@
 #include <string.h>
 
 #define DEBUG 1
+#define URIPARSE_DEBUG 1
+
+// placeholder for an empty string
+char* empty = "";
 
 // proto:[//]authority[/path][?name=ferret[&n1=v1]][#fragment]
-#define NUM_URI_PARTS 6
+#define FOUND_START      0
+#define FOUND_PROTO      1
+#define FOUND_AUTHORITY  2
+#define FOUND_PATH       3
+#define FOUND_QUERY      4
+#define FOUND_FRAGMENT   5
+#define FOUND_END        6
+// how many stages?
+#define NUM_URI_PARTS    6
 
 struct t_uri {
 	char *uri;        // original uri
@@ -18,19 +30,11 @@ struct t_uri {
 	int pos[NUM_URI_PARTS+1]; // -1 == unset
 };
 
-int parse(char* uri, struct t_uri* uri_parsed, char* empty) {
+int parse(char* uri, struct t_uri* uri_parsed) {
 	int doubleslash = 0;
 	int stage = 1;
 	int i = 0;
 	int length = strlen(uri);
-	
-	#define FOUND_START      0
-	#define FOUND_PROTO      1
-	#define FOUND_AUTHORITY  2
-	#define FOUND_PATH       3
-	#define FOUND_QUERY      4
-	#define FOUND_FRAGMENT   5
-	#define FOUND_END        6
 	
 	//struct t_uri uri_parsed = {uri, empty, empty, empty, empty, empty, {-1, -1, -1, -1, -1, -1, -1}};
 	uri_parsed->pos[0] = 0;
@@ -38,13 +42,19 @@ int parse(char* uri, struct t_uri* uri_parsed, char* empty) {
 	
 	for (i=0; i<length; i++) {
 		
+		#if DEBUG > 3
 		printf("[%04d] %c %d\n", i, uri[i], stage);
+		#endif
 		if (stage < FOUND_AUTHORITY && uri[i] == ':') {
 			stage = FOUND_AUTHORITY;
 			uri_parsed->pos[FOUND_PROTO] = i;
+			#if DEBUG > 3
 			printf(", found proto\n");
+			#endif
 			if (uri[i+1] == '/' && uri[i+2] == '/') {
+				#if DEBUG > 3
 				printf(", have double slash\n");
+				#endif
 				i=i+3;
 				doubleslash = 2;
 			}
@@ -56,7 +66,9 @@ int parse(char* uri, struct t_uri* uri_parsed, char* empty) {
 			uri_parsed->pos[FOUND_PATH] = i;
 			if (length == i+1)
 				uri_parsed->pos[FOUND_PATH]++;
+			#if DEBUG > 3
 			printf(", found path\n");
+			#endif
 			continue;
 		}
 
@@ -65,7 +77,9 @@ int parse(char* uri, struct t_uri* uri_parsed, char* empty) {
 			uri_parsed->pos[FOUND_QUERY] = i;
 			if (length == i+1)
 				uri_parsed->pos[FOUND_QUERY]++;
+			#if DEBUG > 3
 			printf(", found query\n");
+			#endif
 			continue;
 		}
 
@@ -74,7 +88,9 @@ int parse(char* uri, struct t_uri* uri_parsed, char* empty) {
 			uri_parsed->pos[FOUND_FRAGMENT] = i;
 			if (length == i+1)
 				uri_parsed->pos[FOUND_FRAGMENT]++;
+			#if DEBUG > 3
 			printf(", found fragment\n");
+			#endif
 			continue;
 		}
 
@@ -102,13 +118,15 @@ int parse(char* uri, struct t_uri* uri_parsed, char* empty) {
 	}
 	
 	// debug output
-	printf("Start     [%d]\n", uri_parsed->pos[FOUND_START]);
-	printf("Proto     [%d]\n", uri_parsed->pos[FOUND_PROTO]);
-	printf("Authority [%d]\n", uri_parsed->pos[FOUND_AUTHORITY]);
-	printf("Path      [%d]\n", uri_parsed->pos[FOUND_PATH]);
-	printf("Query     [%d]\n", uri_parsed->pos[FOUND_QUERY]);
-	printf("Fragment  [%d]\n", uri_parsed->pos[FOUND_FRAGMENT]);
-	printf("End       [%d]\n", uri_parsed->pos[FOUND_END]);
+	#if DEBUG > 1
+	printf("Start      [%d]\n", uri_parsed->pos[FOUND_START]);
+	printf("Proto      [%d]\n", uri_parsed->pos[FOUND_PROTO]);
+	printf("Authority  [%d]\n", uri_parsed->pos[FOUND_AUTHORITY]);
+	printf("Path       [%d]\n", uri_parsed->pos[FOUND_PATH]);
+	printf("Query      [%d]\n", uri_parsed->pos[FOUND_QUERY]);
+	printf("Fragment   [%d]\n", uri_parsed->pos[FOUND_FRAGMENT]);
+	printf("End        [%d]\n", uri_parsed->pos[FOUND_END]);
+	#endif
 
 	// check for required parameters
 	if (uri_parsed->pos[FOUND_PROTO] == -1 ) 
@@ -128,11 +146,13 @@ int parse(char* uri, struct t_uri* uri_parsed, char* empty) {
 	        uri_parsed->pos[FOUND_AUTHORITY]-uri_parsed->pos[FOUND_PROTO]-doubleslash-1);
 	uri_parsed->authority[uri_parsed->pos[FOUND_AUTHORITY]-uri_parsed->pos[FOUND_PROTO]-doubleslash-1] = '\0';
 	
+	#if DEBUG > 1
 	printf("proto:     %s\n", uri_parsed->proto);
 	printf("authority: %s\n", uri_parsed->authority);
+	#endif
 	
+	int len;
 	if (uri_parsed->pos[FOUND_PATH] != -1) {
-		int len = 0;
 		
 		if (uri_parsed->pos[FOUND_QUERY] != -1)
 			len = uri_parsed->pos[FOUND_QUERY] - uri_parsed->pos[FOUND_PATH];
@@ -141,27 +161,85 @@ int parse(char* uri, struct t_uri* uri_parsed, char* empty) {
 		else
 			len = length - uri_parsed->pos[FOUND_PATH];
 		
-		printf("len: %d\n", len);
-		
 		uri_parsed->path = malloc(sizeof(char*) * (length+1));
 		strncpy(uri_parsed->path, 
 				uri_parsed->uri+uri_parsed->pos[FOUND_PATH]+1, // skip '/'
 				len);
 		uri_parsed->path[len-1] = '\0';
 	}
-	printf("path:     %s\n", uri_parsed->path);
+	#if DEBUG > 1
+	printf("path:      %s\n", uri_parsed->path);
+	#endif
+	
+	if (uri_parsed->pos[FOUND_QUERY] != -1) {
+		
+		if (uri_parsed->pos[FOUND_FRAGMENT] != -1)
+			len = uri_parsed->pos[FOUND_FRAGMENT] - uri_parsed->pos[FOUND_QUERY];
+		else
+			len = length - uri_parsed->pos[FOUND_QUERY];
+		
+		uri_parsed->query = malloc(sizeof(char*) * (length+1));
+		strncpy(uri_parsed->query, 
+				uri_parsed->uri+uri_parsed->pos[FOUND_QUERY]+1, // skip '?'
+				len);
+		uri_parsed->query[len-1] = '\0';
+	}
+	#if DEBUG > 1
+	printf("query:     %s\n", uri_parsed->query);
+	#endif
+	
+	if (uri_parsed->pos[FOUND_FRAGMENT] != -1) {
+		
+		len = length - uri_parsed->pos[FOUND_FRAGMENT];
+		
+		uri_parsed->fragment = malloc(sizeof(char*) * (length+1));
+		strncpy(uri_parsed->fragment, 
+				uri_parsed->uri+uri_parsed->pos[FOUND_FRAGMENT]+1, // skip '#'
+				len);
+		uri_parsed->fragment[len-1] = '\0';
+	}
+	#if DEBUG > 1
+	printf("fragment:  %s\n", uri_parsed->fragment);
+	#endif
+	
 	return 0;	
 }
 
-#ifdef DEBUG 
-#if DEBUG > 0
+#ifdef URIPARSE_DEBUG
+#if URIPARSE_DEBUG > 0
 int uri_test(char* uri, char* proto, char* authority, char* path, char* query, char* fragment) {
+	int res, r;
+	struct t_uri uri_parsed = {uri, empty, empty, empty, empty, empty, {-1, -1, -1, -1, -1, -1, -1}};
+	res = parse(uri, &uri_parsed);
+
+	printf("%-10s, %03d, check: '%s', result: '%s'\n", "proto", strcmp(uri_parsed.proto, proto), proto, uri_parsed.proto);
+	printf("%-10s, %03d, check: '%s', result: '%s'\n", "authority", strcmp(uri_parsed.authority, authority), authority, uri_parsed.authority);
+	printf("%-10s, %03d, check: '%s', result: '%s'\n", "path", strcmp(uri_parsed.path, path), path, uri_parsed.path);
+	printf("%-10s, %03d, check: '%s', result: '%s'\n", "query", strcmp(uri_parsed.query, query), query, uri_parsed.query);
+	printf("%-10s, %03d, check: '%s', result: '%s'\n", "fragment", strcmp(uri_parsed.fragment, fragment), fragment, uri_parsed.fragment);
+	
+	if (strcmp(uri_parsed.proto, proto) != 0)
+		return FOUND_PROTO;
+	
+	if (strcmp(uri_parsed.authority, authority) != 0)
+		return FOUND_AUTHORITY;
+	
+	if (strcmp(uri_parsed.path, path) != 0)
+		return FOUND_PATH;
+	
+	if (strcmp(uri_parsed.query, query) != 0)
+		return FOUND_PATH;
+	
+	if (strcmp(uri_parsed.query, query) != 0)
+		return FOUND_QUERY;
+	
+	if (strcmp(uri_parsed.fragment, fragment) != 0)
+		return FOUND_FRAGMENT;
+	
 	return 0;
 }
 
 int main(int argc, char *argv[]) {
-	
-	char* empty = "";
 	
 	// parse an example uri onto the following parts
 	char *uri;
@@ -172,11 +250,18 @@ int main(int argc, char *argv[]) {
 	//uri_parsed.pos[0] = 0;
 	//uri_parsed.pos[FOUND_END] = length;
 	
-	int res, r;
-	uri = "proto:authority/p?a";
+	/*
 	struct t_uri uri_parsed = {uri, empty, empty, empty, empty, empty, {-1, -1, -1, -1, -1, -1, -1}};
-	res = parse(uri, &uri_parsed, empty);
-	r = uri_test(uri, "proto", "authority", "", "a", "");
+	res = parse(uri, &uri_parsed);
+	*/
+	int r;
+	uri = "proto:authority/p?a#f";
+	r = uri_test(uri, "proto", "authority", "p", "a", "f");
+	if (r == 0) {
+		printf("OK     %s\n", uri);
+	} else {
+		printf("Failed %s\n", uri);
+	}
 }
 #endif
 #endif
