3f4c427c7bfc884ee30e156c3c975ab1de097075
Simon Wunderlin <swunderlin@gmail.com>
2017-09-20 08:39:16 +0200

    added working regex example with new mingw distro

--- test/regex.cpp
+++ test/regex.cpp
@@ -2,12 +2,12 @@
 #include <string>
 #include <regex> // std::regex
 #include <pcrecpp.h> // pcrecpp::RE -- needs "-lpcrecpp -lpcre"
-#include <pcrscpp.h> // pcrscpp::replace -- needs "-lpcrscpp -lpcre"
+//#include <pcrscpp.h> // pcrscpp::replace -- needs "-lpcrscpp -lpcre"
 
 int main() {
     std::regex std_rx (R"del(XYZ\d\d)del");
     pcrecpp::RE pcrecpp_rx (R"del(XYZ\d\d)del");
-    pcrscpp::replace pcrscpp_rs(R"del(s/XYZ\d\d/A/g)del");
+    //pcrscpp::replace pcrscpp_rs(R"del(s/XYZ\d\d/A/g)del");
     std::string str = " XYZ111 d-dxxxxxxx XYZ222 t-nyyyyyyyyy XYZ333 t-r ";
 
     std::cout << "std::regex way: " << std::regex_replace (str, std_rx, "A") << std::endl
@@ -16,11 +16,11 @@ int main() {
     std::string buffer(str);
     pcrecpp_rx.GlobalReplace("A", &buffer);
 
-    std::cout << buffer << std::endl
-              << "pcrscpp way: ";
+    //std::cout << buffer << std::endl
+    //          << "pcrscpp way: ";
 
-    pcrscpp_rs.replace_store(str);
-    std::cout << pcrscpp_rs.replace_result << std::endl;
+    //pcrscpp_rs.replace_store(str);
+    //std::cout << pcrscpp_rs.replace_result << std::endl;
 
     return 0;
 }
