c8acba6d3e6f6302741b1ac256d1c35dd37ba442
Simon Wunderlin <swunderlin@gmail.com>
2017-09-20 20:41:35 +0200

    added cmd line interface

--- test/reg.c
+++ test/reg.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include "regcpp.h"
 
-int main() {
-	reg("reg", "AAAAAAAA", "this is a regex text");
+int main(int argc, char* argv[]) {
+	// reg(" +(reg)", "AAAAAAAA '$1' ", "this is a regex text");
+	int ret = reg(argv[1], argv[2], argv[3]);
 	return 0;
 }
--- test/reg.cpp
+++ test/reg.cpp
@@ -12,11 +12,18 @@
 using namespace std;
 
 EXTERNC int reg(const char* areg, const char* areplace, const char* str) {
-	std::regex re(areg);
+	string res = "";
 	string reg(areg);
 	string replace(areplace);
-	cout << reg << " " << replace << endl;
-	string res = std::regex_replace(str, re, replace);
+	
+	try {
+		std::regex re(areg);
+		cout << reg << " " << replace << endl;
+		res = regex_replace(str, re, replace);
+	} catch (regex_error e) {
+		return e.code();
+	}
+	
 	cout << "Result: " << res << endl;
 	return 0;
 }
