caf5cdeb11cbae8d11362b036b913221fa44b02c
Wunderlin Simon <Simon.Wunderlin@usb.ch>
2017-09-21 07:49:23 +0200

    some regex cleanup

--- test/reg.c
+++ test/reg.c
@@ -4,5 +4,8 @@
 int main(int argc, char* argv[]) {
 	// reg(" +(reg)", "AAAAAAAA '$1' ", "this is a regex text");
 	int ret = reg(argv[1], argv[2], argv[3]);
+	if (ret != 0) {
+		printf("Failed to execute regex: %d\n", ret);
+	}
 	return 0;
 }
--- test/reg.cpp
+++ test/reg.cpp
@@ -11,17 +11,35 @@
 
 using namespace std;
 
+// Currently, the following modifiers are supported
+//
+//    modifier              description               Perl corresponding
+//
+//    PCRE_CASELESS         case insensitive match    /i
+//    PCRE_MULTILINE        multiple lines match      /m
+//    PCRE_DOTALL           dot matches newlines      /s
+//    PCRE_DOLLAR_ENDONLY   $ matches only at end     N/A
+//    PCRE_EXTRA            strict escape parsing     N/A
+//    PCRE_EXTENDED         ignore whitespaces        /x
+//    PCRE_UTF8             handles UTF8 chars        built-in
+//    PCRE_UNGREEDY         reverses * and *?         N/A
+//    PCRE_NO_AUTO_CAPTURE  disables matching parens  N/A (*)
+//
+// TODO: implement re options
+// TODO: implement parser that takes a regex string like /re/replace/modifiers
+
+int reg_error_code = 0;
 EXTERNC int reg(const char* areg, const char* areplace, const char* str) {
 	string res = "";
-	string reg(areg);
 	string replace(areplace);
 	
 	try {
-		std::regex re(areg);
-		cout << reg << " " << replace << endl;
-		res = regex_replace(str, re, replace);
+		regex re(areg);
+		cout << areg << " " << areplace << endl;
+		res = regex_replace(str, re, areplace);
 	} catch (regex_error e) {
-		return e.code();
+		reg_error_code = e.code();
+		return reg_error_code;
 	}
 	
 	cout << "Result: " << res << endl;
