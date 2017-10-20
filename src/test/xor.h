9c1fab27f30d41e3513ff1837a8dc7bac9a71cae
Simon Wunderlin <swunderlin@gmail.com>
2017-10-20 20:14:38 +0200

    aaded

---
+++ src/test/xor.h
@@ -0,0 +1,19 @@
+#ifndef __XOR_H__
+#define __XOR_H__
+
+/* Make this header file easier to include in C++ code */
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef enum {DECODE, ENCODE} xormode_t;
+
+char *xor(char *string, const char *key, long l);
+int transcode_file(char* fin, char* fout, char* key);
+int transcode_str(char* strin, long* fsize, char** strout, char* key);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __XOR_H__ */
