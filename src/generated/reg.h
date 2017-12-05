6b5e41f900a1a2171713cc8bdd6a2e8af12ed651
Wunderlin Simon <WunderlinS@ms.uhbs.ch>
2017-12-05 11:32:18 +0100

    added meona example

--- src/generated/reg.h
+++ src/generated/reg.h
@@ -1,27 +1,27 @@
 const char* reg_str = ""
-"Windows Registy Edito Vesion 5.00\n"
-";; Registe a potocol handle\n"
-";;\n"
-";; REPLACE <poto> with you potocol name and <youpath> with you path \n"
-";; to the executable\n"
-"\n"
-"[HKEY_CLASSES_ROOT\\<poto>]\n"
-"@=\"URL: <poto> Potocol\"\n"
-"\"URL Potocol\"=\"\"\n"
-"\n"
-"[HKEY_CLASSES_ROOT\\<poto>\\shell]\n"
-"[HKEY_CLASSES_ROOT\\<poto>\\shell\\open]\n"
-"[HKEY_CLASSES_ROOT\\<poto>\\shell\\open\\command]\n"
-"; eplace <youpath> with you installation location\n"
-"@=\"\\\"<youpath>\\\\ph\\\" \\\"%1\\\"\"\n"
-"\n"
-";; intenet exploe will show a waning when this potocol is used, you can \n"
-";; disable this waning site wide o pe use\n"
-"; pe use setting\n"
-";[HKEY_CURRENT_USER\\Softwae\\Micosoft\\Intenet Exploe\\PotocolExecute\\<poto>]\n"
-";\"WanOnOpen\"=dwod:00000000\n"
-"\n"
-"; compute setting\n"
-"[HKEY_LOCAL_MACHINE\\Softwae\\Micosoft\\Intenet Exploe\\PotocolExecute\\<poto>]\n"
-"\"WanOnOpen\"=dwod:00000000\n"
+"Windows Registry Editor Version 5.00\r\n"
+";; Register a protocol handler\r\n"
+";;\r\n"
+";; REPLACE <proto> with your protocol name and <yourpath> with your path \r\n"
+";; to the executable\r\n"
+"\r\n"
+"[HKEY_CLASSES_ROOT\\<proto>]\r\n"
+"@=\"URL: <proto> Protocol\"\r\n"
+"\"URL Protocol\"=\"\"\r\n"
+"\r\n"
+"[HKEY_CLASSES_ROOT\\<proto>\\shell]\r\n"
+"[HKEY_CLASSES_ROOT\\<proto>\\shell\\open]\r\n"
+"[HKEY_CLASSES_ROOT\\<proto>\\shell\\open\\command]\r\n"
+"; replace <yourpath> with your installation location\r\n"
+"@=\"\\\"<yourpath>\\\\ph.exe\\\" \\\"%1\\\"\"\r\n"
+"\r\n"
+";; internet explorer will show a warning when this protocol is used, you can \r\n"
+";; disable this warning site wide or per user\r\n"
+"; per user setting\r\n"
+";[HKEY_CURRENT_USER\\Software\\Microsoft\\Internet Explorer\\ProtocolExecute\\<proto>]\r\n"
+";\"WarnOnOpen\"=dword:00000000\r\n"
+"\r\n"
+"; computer setting\r\n"
+"[HKEY_LOCAL_MACHINE\\Software\\Microsoft\\Internet Explorer\\ProtocolExecute\\<proto>]\r\n"
+"\"WarnOnOpen\"=dword:00000000\r\n"
 "";
