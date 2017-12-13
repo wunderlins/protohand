38c22d3f06d425e2cfa547c56b161cf4466183cb
Simon Wunderlin <swunderlin@gmail.com>
2017-12-13 17:46:11 +0100

    added transform tests

---
+++ src/test/transform.c
@@ -0,0 +1,8 @@
+#include "transform.h"
+
+#ifdef TRANSFORM_MAIN
+int main(int argc, char** argv) {
+	
+	return 0;
+}
+#endif
---
+++ src/test/transform.h
@@ -0,0 +1,15 @@
+#ifndef __TRANSFORM_H__
+#define __TRANSFORM_H__
+
+/* Make this header file easier to include in C++ code */
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdio.h>
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __NVLIST_H__ */
