a75cfe87ec2895aae232b0142a9863781cd03c15
wus <wus@shell5.intra.wunderlin.net>
2017-12-15 16:51:54 +0100

    added function registry

--- src/test/transform.c
+++ src/test/transform.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 #define DEFAULT_TRANSFORM_ITEM {"", "", NULL, "", 0}
-typedef struct {
+typedef struct transform_item {
 	char* name;
 	char* value_in; 
 	char* value_out; 
@@ -11,7 +11,7 @@ typedef struct {
 	int transformed; 
 } transform_item_t;
 
-typedef struct {
+typedef struct transform_list {
 	transform_item_t *items;
 	int length;
 } transform_list_t;
@@ -23,20 +23,24 @@ int get_function_def(struct nvlist_list *func) {
 	if (ret < 0) return ret;
 	ret = nvlist_addpair(func, "patnr_zero", "printf(%08d)");
 	if (ret < 0) return ret;
-	ret = nvlist_addpair(func, "fallnr_nozero", "str2int");
+	ret = nvlist_addpair(func, "fallnr_nozero", "ltrimzero");
 	if (ret < 0) return ret;
-	ret = nvlist_addpair(func, "patnr_nozero", "str2int");
+	ret = nvlist_addpair(func, "patnr_nozero", "ltrimzero");
 	if (ret < 0) return ret;
 	
 	return 0;
 }
 
-typedef struct {
+typedef struct fn {
 	char* name;
 	int   numargs;
-	char** arguments;
 	void (*func)(char* value, ...);
 } fn_t;
+
+typedef struct fn_list {
+	fn_t *items;
+	int length;
+} fn_list_t;
 
 
 void my_printf(char *format, ...) {
@@ -44,6 +48,24 @@ void my_printf(char *format, ...) {
 	va_start(args, format);
 	vprintf(format, args);
 	va_end(args);
+}
+
+int add_func(fn_list_t* funcs, fn_t* fnc) {
+	if (funcs->length == 0) {
+		funcs->items = malloc(sizeof(fn_t) * 1);
+		if (funcs->items == NULL)
+			return -1;
+		funcs->length = 1;
+	} else {
+		funcs->items = realloc(funcs->items, sizeof(fn_t) * (funcs->length+1));
+		if (funcs->items == NULL)
+			return -1;
+		funcs->length++;
+	}
+	
+	funcs->items[funcs->length - 1] = *fnc;
+	
+	return 0;
 }
 
 #ifdef TRANSFORM_MAIN
@@ -54,13 +76,20 @@ int main(int argc, char** argv) {
 	//printf("func length %d\n", func.length);
 	
 	//typedef int (*fn_printf_ptr) (char const *str, ...);
-	fn_t fn_printf = {"printf", -1, NULL, NULL};
+	fn_t fn_printf = {"printf", -1, NULL};
+	fn_printf.numargs = 1;
 	//fn_printf.func = &printf;
 	
 	//void (*ptr_printf)(char *str, ...);
-	fn_printf.func = &my_printf;
+	fn_printf.func = &my_printf;	
 	
-	fn_printf.func("blah, %d\n", 1);
+	fn_list_t funcs;
+	funcs.length = 0;
+	add_func(&funcs, &fn_printf);
+	//add_func(&funcs, &fn_printf);
+	 
+	funcs.items[0].func("blah, %08d\n", 1);
+	
 	
 	struct nvlist_list rep = nvlist_create(2);
 	
