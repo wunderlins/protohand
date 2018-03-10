3f6090f3d94f86ab319d0340c1ea4102cb90a498
Simon Wunderlin <swunderlin@gmail.com>
2018-03-10 17:43:34 +0100

    added cmd splitter, problems with unicode characters

---
+++ src/test/parsecmd.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <windows.h>
+
+int parse_wcmd(char* in, LPWSTR **args) {
+	int i, c;
+	size_t s = 1 + strlen(in);
+	wchar_t inw[s];
+	mbstowcs(inw, in, s);
+	*args = CommandLineToArgvW(inw, &c);
+	return c;
+}
+
+int main(int argc, char** argv) {
+	printf("%ls\n", argv[1]); 
+	
+	int i;
+	LPWSTR *a;
+	int c = parse_wcmd(GetCommandLine(), &a);
+	/*
+	if (c)
+		for (i = 0; i < c; i++) 
+			printf("%d=%ls\n", i, a[i]); 
+	*/
+	
+	LPWSTR exe = a[0];
+	LPWSTR args[c];
+	for (i=1; i<c; i++) {
+		args[i-1] = a[i];
+		printf("%d=%ls\n", i, a[i]); 
+	}
+	
+	return 0;
+}
