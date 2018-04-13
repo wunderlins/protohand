fc601611021b681c5803ee63d2e8433904c661ab
Simon Wunderlin <swunderlin@gmail.com>
2018-04-13 16:42:01 +0200

    added tests for split_arg()

--- src/test/split_arg.c
+++ src/test/split_arg.c
@@ -3,52 +3,6 @@
 #include <stdlib.h>
 #include "../lib/stringlib.h"
 
-int split_arg(char* str, char*** args) {
-	size_t i = 0;
-	size_t len = strlen(str);
-	int open = 0;
-	char buffer[2048] = {0};
-	int buff_len = 0;
-	int arg_len = 0;
-	*args = malloc(sizeof(*args)*1024);
-	
-	for (i=0; i<len; i++) {
-		if (str[i] == '"') {
-			open = (open) ? 0 : 1;
-			continue;
-		}
-		
-		if (str[i] == ' ' && !open) {
-			printf("%s\n", buffer);
-			(*args)[arg_len] = (char*) malloc(sizeof(char)*(strlen(buffer)+1));
-			char *p = &(buffer)[0];
-			cmdunquote(&p);
-			strcpy((*args)[arg_len], buffer);
-			arg_len++;
-			//args++;
-			buffer[0] = 0;
-			buff_len = 0;
-			continue;
-		}
-		
-		buffer[buff_len++] = str[i];
-		buffer[buff_len] = 0;
-		
-		//printf("%c", str[i]);
-	}
-	
-	if(str[i] == 0 && i) {
-		(*args)[arg_len] = (char*) malloc(sizeof(char)*(strlen(buffer)+1));
-		char *p = &(buffer)[0];
-		cmdunquote(&p);
-		strcpy((*args)[arg_len], buffer);
-		arg_len++;
-		printf("%s\n", buffer);
-	}
-	
-	return arg_len;
-}
-
 int main() {
 	int i, len;
 	char** ar;
@@ -60,26 +14,34 @@ int main() {
 	len = split_arg(str1, &ar);
 	printf("len: %d, %s\n", len, str1);
 	for (i=0; i<len; i++) {
-		printf("%d: %s\n", i, ar[i]);
+		printf("%d: %s, ", i, ar[i]);
 	}
+	printf("\n");
+	if (len != 3) return 1;
 	
 	len = split_arg(str2, &ar);
 	printf("len: %d, %s\n", len, str2);
 	for (i=0; i<len; i++) {
-		printf("%d: %s\n", i, ar[i]);
+		printf("%d: %s, ", i, ar[i]);
 	}
+	printf("\n");
+	if (len != 0) return 1;
 	
 	len = split_arg(str3, &ar);
 	printf("len: %d, %s\n", len, str3);
 	for (i=0; i<len; i++) {
-		printf("%d: %s\n", i, ar[i]);
+		printf("%d: %s, ", i, ar[i]);
 	}
+	printf("\n");
+	if (len != 2) return 1;
 	
 	len = split_arg(str4, &ar);
 	printf("len: %d, %s\n", len, str4);
 	for (i=0; i<len; i++) {
-		printf("%d: %s\n", i, ar[i]);
+		printf("%d: %s, ", i, ar[i]);
 	}
+	printf("\n");
+	if (len != 1) return 1;
 	
 	return 0;
 }
